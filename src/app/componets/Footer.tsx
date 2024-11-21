0f866a0ff6d7424f7324e3109b4010b508196cff
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-11-21 18:41:51 -0400

    actualizacion nuevos componentes y estilos del portfolio

---
+++ src/app/componets/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <h2>Footer</h2>    
+    </footer>
+  )
+}
