0ebf86654d3f82a2bd4ae28a667c3c6f55d3e5d8
yessenia22c <yesseniavillarte@gmail.com>
2024-11-22 15:25:39 -0400

    chore: Update page layout and font size in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Content } from "./componets/Content";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center ">
-      <div className="flex flex-col w-[70%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
+    <div className=" flex flex-col items-center">
+      <div className="flex flex-col w-[50%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
