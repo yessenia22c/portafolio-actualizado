df8760d8a703daf4ba285861bbbcd00c71d7368b
yessenia22c <yesseniavillarte@gmail.com>
2024-11-23 12:28:28 -0400

    Update page layout and font size in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import { Footer } from "./componets/Footer";
-import { Header } from "./componets/Header";
-import { Content } from "./componets/Content";
+import { Footer } from "../componets/Footer";
+import { Header } from "../componets/Header";
+import { Content } from "../componets/Content";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center">
-      <div className="flex flex-col w-[50%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
-      <Header></Header>
-      <Content></Content>
-      <Footer></Footer>
-    </div>
+    <div className="bg-slate-50 dark:bg-[#001c2a] flex flex-col items-center">
+        <div className="flex flex-col w-[50%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
+          <h1>Hola</h1>
+          <Header></Header>
+          <Content></Content>
+          <Footer></Footer>
+        </div>
     </div>
   );
 }
