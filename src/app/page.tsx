5a54a7ed164d2ed318458163858f828305b2cb84
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-11-23 21:27:29 -0400

    feat: Update metadata titles and descriptions; fix component import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import { Footer } from "../componets/Footer";
-import { Header } from "../componets/Header";
-import { Content } from "../componets/Content";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Content } from "../components/Content";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 dark:bg-[#001c2a] flex flex-col items-center">
-        <div className="flex flex-col w-[50%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
-          <h1>Hola</h1>
+        <div className="flex flex-col w-[70%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
           <Header></Header>
           <Content></Content>
           <Footer></Footer>
