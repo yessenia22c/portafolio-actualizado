7cd46ae0ed00718de56493c77336ccffb082ebf7
yessenia22c <yesseniavillarte@gmail.com>
2024-11-25 16:04:00 -0400

    feat: Add CSS file for ThemeToggle component and update Toggle.css import

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useTheme } from "@/Hooks/useTheme";
+import './css/Toggle.css'
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
@@ -12,7 +13,30 @@ export default function ThemeToggle() {
         checked={theme === 'dark'}
         onChange={toggleTheme}
       />
-      <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#00ba95] "></div>
     </label>
   );
+}
+
+export  function ThemeToggle2() {
+  const { theme, toggleTheme } = useTheme();
+ return (
+  <label htmlFor="theme" className="theme">
+	<span className="theme__toggle-wrap">
+		<input id="theme" className="theme__toggle" checked={theme === 'dark'} onChange={toggleTheme}  type="checkbox" role="switch" name="theme" value="dark"/>
+		<span className="theme__fill"></span>
+		<span className="theme__icon">
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+			<span className="theme__icon-part"></span>
+		</span>
+	</span>
+</label>
+ )
 }
