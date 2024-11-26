7a0bba3fd0285a0cc39f265d94566fc795995c0b
yessenia22c <yesseniavillarte@gmail.com>
2024-11-26 12:03:01 -0400

    feat: Update Header component with responsive navigation menu
    This commit updates the Header component to include a responsive navigation menu. The menu now displays different options based on the screen size, improving the user experience on both small and large devices. Additionally, the ThemeToggle2 import has been added to enable theme switching functionality. These changes enhance the navigation and functionality of the application.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import ThemeToggle, { ThemeToggle2 } from './ThemeToggle'
+import { MenuDesplegable } from './MenuDesplegable'
 
 export const Header = () => {
   return (
     <header className='w-full'>
-        <nav className='flex gap-8 text-sm bg-gradient-to-b from-[#1194ff3a] to-[#00132e]   border items-center justify-center border-[#00ba95] rounded-full px-2 py-2'>
-            <ul className='flex gap-8 items-center'>
+        <nav className='flex gap-8 text-sm bg-gradient-to-b dark:from-[#1194ff3a] dark:to-[#00132e]  border items-center justify-center border-gray-800 dark:border-[#00ba95] rounded-full px-2 py-2'>
+            <ul className='max-[532px]:hidden flex gap-8 items-center text-gray-800 dark:text-white'>
                 <li><a href="#sobre-mi">Sobre mí</a></li>
+                <li><a href="#experiencia">Experiencia</a></li>
                 <li><a href="#proyectos">Proyectos</a></li>
-                <li><a href="#habilidades">Habilidades</a></li>
-                <li><a href="#contactos">Contactos</a></li>
+                <li><a href="#educacion">Educación</a></li>
+                <li className='flex items-center justify-center'>
+                    <ThemeToggle></ThemeToggle>
+                </li>
+            </ul>
+            <ul className='min-[532px]:hidden flex gap-8 items-center text-gray-800 dark:text-white'>
+              <li>
+                <MenuDesplegable></MenuDesplegable>
+              </li>
                 <li>
                     <ThemeToggle></ThemeToggle>
                 </li>
