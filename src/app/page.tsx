981efe1a519280d575b606f7c28369aed74e3f19
yessenia22c <yesseniavillarte@gmail.com>
2024-11-26 12:02:32 -0400

    feat: Update page layout to use full width on larger screens
    This commit modifies the page layout in the Home component to use the full width on larger screens. The "w-[70%]" class has been replaced with "w-full md:w-[70%] lg:w-[70%]" to ensure the content spans the entire width of the screen on medium and large devices. This change improves the responsiveness and visual appearance of the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Content } from "../components/Content";
 export default function Home() {
   return (
     <div className="bg-slate-50 dark:bg-[#001c2a] flex flex-col items-center">
-        <div className="flex flex-col w-[70%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
+        <div className="flex flex-col w-full md:w-[70%] lg:w-[70%] items-center justify-items-center min-h-screen gap-16 p-8 font-[family-name:var(--font-geist-sans)]">
           <Header></Header>
           <Content></Content>
           <Footer></Footer>
