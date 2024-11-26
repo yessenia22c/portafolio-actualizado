95f34784ae7debc56b8126048d23eb412a3086c7
yessenia22c <yesseniavillarte@gmail.com>
2024-11-26 12:02:56 -0400

    feat: Update Footer component with new text and styling
    This commit updates the Footer component by replacing the existing text with a new message in Spanish, "Gracias por tu interés | Desarrollado por Yessenia Villarte". It also adds a new CSS class, "dark:text-white", to change the text color to white when using a dark theme. This change enhances the content and visual appearance of the footer section.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <h2>Footer</h2>    
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center dark:text-white text-gray-800">
+        <h2>Gracias por tu interés | Desarrollado por Yessenia Villarte</h2>    
     </footer>
   )
 }
